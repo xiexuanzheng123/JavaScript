a556b3c395f0265b310acda628cc1a65a680a2ef
xiexuanzheng123 <349328984@qq.com>
2018-02-27 17:28:58 +0800

    function scope

---
+++ src/js/scope/functionScope.js
@@ -0,0 +1,43 @@
+/**
+ * 属于这个函数的全部变量都可以在整个函数的范围内使用或复用 
+ */
+
+function doSomething(a) {
+    b = a + doSomethingElse(a * 2);
+
+    console.log(b * 3);
+}
+function doSomethingElse(a) {
+    return a - 1;
+}
+var b;
+
+doSomething(2);
+
+// function foo() {
+//     function bar(a) {
+//         var i = 3;
+//         console.log(a + i);
+//     }
+
+//     for(let i=0; i<10; i++) {
+//         bar(i * 2);
+//     }
+// }
+// foo();
+
+var c = 2;
+(function IIFE(global) {
+    var c = 3;
+    console.log(c);
+    console.log(global);
+})(window);
+
+var d = 2;
+(function IIFE(def) {
+    def(window);
+})(function def(global) {
+    var d = 3;
+    console.log(d);
+    console.log(global.d);
+})
