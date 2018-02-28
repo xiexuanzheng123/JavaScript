8c2fe4980d00d2e9d4054a48b17b86b1237169f9
xiexuanzheng123 <349328984@qq.com>
2018-02-28 11:23:25 +0800

    module demo

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     entry: {
         index: [
             'babel-polyfill',
-            './src/js/closure/for.js'
+            './src/js/closure/module.js'
         ]
     },
     output: {
