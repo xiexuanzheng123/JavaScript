e59de8b9132fba1745dd4933971ce549259788b9
xiexuanzheng123 <349328984@qq.com>
2018-03-01 11:40:20 +0800

    add notation

--- src/js/this_prototype/wrong1.js
+++ src/js/this_prototype/wrong1.js
@@ -2,6 +2,7 @@ function foo(num) {
     console.log('foo: ' + num);
 
     this.count++;//this并不是指向函数本身
+                //可以使用foo来代替this，使用foo.count
 }
 
 foo.count = 0;
@@ -10,7 +11,7 @@ var i;
 
 for(i=0; i<10; i++) {
     if(i > 5) {
-        foo.call(foo, i);
+        foo.call(foo, i);//强制this指向foo函数对象 
     }
 }
 console.log(foo.count);
