d5a0eee344e80f7f3782d3e94b3ac5544d913217
xiexuanzheng123 <349328984@qq.com>
2018-03-18 18:01:46 +0800

    es6 and function to implement delegate

---
+++ src/js/delegate/function.js
@@ -0,0 +1,38 @@
+function Widget(width, height) {
+    this.width = width || 50;
+    this.height = height || 50;
+    this.$elem = null;
+}
+Widget.prototype.render = function($where) {
+    if(this.$elem) {
+        this.$elem.css({
+            width: this.width + 'px',
+            height: this.height + 'px'
+        }).appendTo($where);
+    }
+};
+function Button(width, height, label) {
+    Widget.call(this, width, height);//丑陋的显式伪多态
+    this.label = label || "Default";
+
+    this.$elem = $("<button>").text(this.label);
+}
+
+Button.prototype = Object.create(Widget.prototype);
+
+Button.prototype.render = function($where) {
+    Widget.prototype.render.call(this, $where);//丑陋的显式伪多态
+    this.$elem.click(this.onClick.bind(this));
+};
+
+Button.prototype.onClick = function(evt) {
+    console.log("Button" + this.label + "clicked");
+};
+$(document).ready(function() {
+    var $body = $(document.body);
+    var btn1 = new Button(125, 30, "Hello");
+    var btn2 = new Button(150, 40, "World");
+
+    btn1.render($body);
+    btn2.render($body);
+})
