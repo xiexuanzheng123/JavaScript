05469f243f7ba4fde951a713a7e99209ddf0c091
xiexuanzheng123 <349328984@qq.com>
2018-03-19 18:47:15 +0800

    the method of with

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     entry: {
         index: [
             'babel-polyfill',
-            './src/js/es6/super.js'
+            './src/js/scope/with.js'
         ]
     },
     output: {
