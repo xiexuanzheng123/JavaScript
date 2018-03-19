99ded85e9c2547b77004f2771ec9e4286eeb196a
xiexuanzheng123 <349328984@qq.com>
2018-03-19 18:32:49 +0800

    'notation'

--- src/js/scope/eval.js
+++ src/js/scope/eval.js
@@ -1,6 +1,16 @@
+/**
+ * 欺骗作用域会导致性能下降
+ */
+
 function foo(str, a) {
-    eval(str);//就类似把定义b=3的代码写在这里
-    console.log(a, b);
+    eval(str);//就类似把定义b=3的代码写在这里,如果是严格模式，则无法修改所在的作用域
+    console.log(a, b);//1 3
 }
 var b = 2;
 foo('var b = 3;', 1);//1 3
+
+/**
+ * javascript还有一些其他方法和eval相似
+ * setTimeout
+ * setInterval
+ */
