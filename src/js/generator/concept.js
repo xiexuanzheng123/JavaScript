721200ed26c44df02488a0e153a56e86e2e93663
xiexuanzheng123 <349328984@qq.com>
2018-04-04 17:34:44 +0800

    concept of generator

--- src/js/generator/concept.js
+++ src/js/generator/concept.js
@@ -1,16 +1,27 @@
-var x = 1;
-function *foo() {
-    x++;
-    yield;
-    //console.log("x: " + x);
+// var x = 1;
+// function *foo() {
+//     x++;
+//     yield;
+//     //console.log("x: " + x);
+// }
+
+// function bar() {
+//     x++;
+// }
+// var it = foo();
+// it.next();
+// console.log("x1: ", x);
+// bar();
+// it.next();
+// console.log("x2: ", x);
+/**
+ * 我们只是创建了一个迭代器对象 
+ */
+function *foo(x, y) {
+    return x * y;
 }
 
-function bar() {
-    x++;
-}
-var it = foo();
-it.next();
-console.log("x1: ", x);
-bar();
-it.next();
-console.log("x2: ", x);
+var it = foo(6, 7);
+var res = it.next();
+
+console.log(res.value);
