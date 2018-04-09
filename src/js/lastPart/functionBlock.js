3ecd19986ff12979af9087984a17686053a5323b
xiexuanzheng123 <349328984@qq.com>
2018-04-09 11:24:23 +0800

    function in block

---
+++ src/js/lastPart/functionBlock.js
@@ -0,0 +1,8 @@
+{
+    foo();
+
+    function foo() {
+        console.log('123');
+    }
+}
+//foo();ReferenceError
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     entry: {
         index: [
             'babel-polyfill',
-            './src/js/lastPart/functionArr.js'
+            './src/js/lastPart/functionBlock.js'
         ]
     },
     output: {
