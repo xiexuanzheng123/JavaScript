e00c49bc25ab8ac9d8f7c9c0dd2344f8e35f3d0e
xiexuanzheng123 <349328984@qq.com>
2018-04-09 10:39:17 +0800

    for loop

---
+++ src/js/lastPart/for.js
@@ -0,0 +1,14 @@
+let a = 2;
+
+if (a > 1) {
+    let b = a * 3;
+    console.log(b);
+
+    for (let i = a; i <= b; i++) {
+        let j = i + 10;
+        console.log(j);
+    }
+
+    let c = a + b;
+    console.log(c);
+}
